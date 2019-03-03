3e510512ad8eed8b201aba3eb0e3f8c659b98b7f
schriker <janekmachine@gmail.com>
2019-03-03 17:48:15 +0100

    Bullets animation based on animeJS

--- src/components/Bullets/Bullets.jsx
+++ src/components/Bullets/Bullets.jsx
@@ -1,27 +1,40 @@
-import React, { useEffect, useContext } from 'react'
+import React, { useEffect, useContext, useRef } from 'react'
 import { StateContext } from '../../store/store'
 import styles from './Bullets.module.css'
+import anime from 'animejs'
 import * as actionTypes from '../../store/actionTypes'
 
 const Bullets = (props) => {
 
+  const element = useRef()
   const store = useContext(StateContext)
 
   useEffect(() => {
-      const payload =  {
-        index: props.index,
-        bullet: {
-          type: props.type,
-          position: {
-            x: props.x,
-            y: props.y - 5
+    anime({
+      targets: element.current,
+      translateY: -store.state.game.arenaHeight,
+      duration: 1000,
+      easing: 'linear',
+      change() {
+        // const rect = element.current.getBoundingClientRect()
+      },
+      complete(anim) {
+        if (anim.completed) {
+            const payload =  {
+            index: props.index,
+            bullet: {
+              type: props.type,
+              position: {
+                x: 0,
+                y: 0
+              }
+            }
           }
+          store.dispatch({type: actionTypes.BULLET_MOVE, payload})
         }
       }
-      if (props.y > -28) {
-        store.dispatch({type: actionTypes.BULLET_MOVE, payload})
-      }
-  }, [store])
+    })
+  }, [])
 
   const style = {
     left: props.x + 'px',
@@ -30,8 +43,8 @@ const Bullets = (props) => {
 
   let bullet = null
 
-  if (props.y > -28) {
-    bullet = <div style={style} className={`${styles.body} ${styles[props.type]}`}></div>
+  if (props.y > 0) {
+    bullet = <div ref={element} style={style} className={`${styles.body} ${styles[props.type]}`}></div>
   }
 
   return bullet
