96ea704f2f8d05a29796996feac43fc5593186bc
Marcin Janus <marcinjanus@MacBook-Pro-Marcin.local>
2019-03-24 07:51:24 +0100

    Updates

---
+++ precache-manifest.ffb7d64cc632a988177c81e5d0f47892.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "f2458ac906c0026bdd03748d3e9a4d5d",
+    "url": "/spacjo/static/media/platform.f2458ac9.png"
+  },
+  {
+    "revision": "843abc671adc669b45e794061472b780",
+    "url": "/spacjo/static/media/logo.843abc67.png"
+  },
+  {
+    "revision": "8d66e0ed13e40978334ab27aeac03e01",
+    "url": "/spacjo/static/media/bg.8d66e0ed.jpg"
+  },
+  {
+    "revision": "ae65df042714c009a92f91aca240ca1d",
+    "url": "/spacjo/static/media/battleship_red.ae65df04.png"
+  },
+  {
+    "revision": "96e4396edf3284fd21bad89dbd5d378d",
+    "url": "/spacjo/static/media/battleship_blue.96e4396e.png"
+  },
+  {
+    "revision": "c8c5a0935ebb37c7eb0b",
+    "url": "/spacjo/static/js/runtime~main.c8c5a093.js"
+  },
+  {
+    "revision": "c2f3a5a72c420747567d",
+    "url": "/spacjo/static/js/main.c2f3a5a7.chunk.js"
+  },
+  {
+    "revision": "89b4f4b8116fc8fe487f",
+    "url": "/spacjo/static/js/2.89b4f4b8.chunk.js"
+  },
+  {
+    "revision": "c2f3a5a72c420747567d",
+    "url": "/spacjo/static/css/main.2fffe902.chunk.css"
+  },
+  {
+    "revision": "081d1d720e416c20e2c7a836a718e1f0",
+    "url": "/spacjo/index.html"
+  }
+];
